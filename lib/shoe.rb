67dbd4d59abb77b7c2fe7951cce7cbdaebf0599e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-22 01:06:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 class Shoes
-  attr_accessor :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
   end 
+  
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 end 
